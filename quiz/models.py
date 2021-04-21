c2a0b593ef51e51125433b046c4f7ce8a812e0ea
Patrizio <pangelini@johncabot.edu>
2021-04-21 15:13:51 +0200

    modified order of Question and Answer in the models.py

--- quiz/models.py
+++ quiz/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models 
+class Question(models.Model):
+        text = models.CharField(max_length=30);
+        def __str__(self):
+            return self.text
 
-
-class Answer(models.Models):
+class Answer(models.Model):
         text = models.CharField(max_length=30);
         correct = models.BooleanField();
         question = models.ForeignKey(Question, on_delete=models.CASCADE);
@@ -13,8 +15,3 @@ class Answer(models.Models):
             return self.text
        
         
-class Question(models.Models):
-        text = models.CharField(max_length=30);
-        def __str__(self):
-            return self.text
-    
