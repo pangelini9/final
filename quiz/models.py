453e2bbf7363b5f379d1c8015fb38c002660dd4d
locorrera <locorrera@gmail.com>
2021-04-21 15:07:39 +0200

    Added Question and Answer Classes

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models 
+
+
+class Answer(models.Models):
+        text = models.CharField(max_length=30);
+        correct = models.BooleanField();
+        question = models.ForeignKey(Question, on_delete=models.CASCADE);
+        def __str__(self):
+            return self.text
+       
+        
+class Question(models.Models):
+        text = models.CharField(max_length=30);
+        def __str__(self):
+            return self.text
+    
