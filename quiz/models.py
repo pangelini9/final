96cf214aeeff840a40d24f93f3b7d71235822232
pangelini@johncabot.edu <pangelini@johncabot.edu>
2021-04-26 16:20:58 +0200

    changes in the lecture

--- quiz/models.py
+++ quiz/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
-
-class Attempt(models.Model):
-    ### made by user
-    attempt_time = models.DurationField()
-    #which_quiz = models.FilePathField(path=)
-
-class Quiz(models.Model):
-        text = models.CharField(max_length=30)
-        def __str__(self):
-            return self.text
 
 class Question(models.Model):
         text = models.CharField(max_length=60);
@@ -24,4 +16,20 @@ class Answer(models.Model):
         def __str__(self):
             return self.text
        
-        
+class Quiz(models.Model):
+        text = models.CharField(max_length=30)
+        questions = models.ManyToManyField(Question)
+        def __str__(self):
+            return self.text
+
+class Attempt(models.Model):
+    ### made by user
+    attempt_time = models.DateTimeField()
+    # list of selected answers
+    score = models.IntegerField()
+    feedback = models.CharField(max_length=200)
+    quiz = models.ForeignKey(Quiz, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+    
+    #which_quiz = models.FilePathField(path=)
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+import random
+
 # Create your views here.
 
 def home(request):
     return render(request, 'quiz/index.html');
+
+
+# random.sample(list, number)
+# returns a new list that is composed of "number" elements 
+# taken at random from the list "list"
+
