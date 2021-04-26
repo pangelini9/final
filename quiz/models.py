65cc6fdadd4c306d1753183a72fb2822b1bf4267
locorrera <locorrera@gmail.com>
2021-04-26 14:28:24 +0200

    Increased number of characters for the questions

--- quiz/models.py
+++ quiz/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
-        text = models.CharField(max_length=30);
+        text = models.CharField(max_length=60);
         def __str__(self):
             return self.text
 
 class Answer(models.Model):
-        text = models.CharField(max_length=30);
+        text = models.CharField(max_length=60);
         correct = models.BooleanField();
         question = models.ForeignKey(Question, on_delete=models.CASCADE);
         def __str__(self):
