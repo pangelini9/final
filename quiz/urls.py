0c1eb00247a98f626c4991333225f4822270d3d6
pangelini9 <pangelini@johncabot.edu>
2021-04-29 20:22:28 +0200

    set up the files for last meeting

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,4 +6,6 @@ app_name = "quiz"
 
 urlpatterns = [
     path("", views.home, name = "home"),
+#    path("attempt", views.attempt, name = "attempt"),
+    path("attempt/<int:student>", views.attempt, name = "attempt"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Question
+
 import random
 
 # Create your views here.
@@ -12,3 +14,33 @@ def home(request):
 # returns a new list that is composed of "number" elements 
 # taken at random from the list "list"
 
+def attempt(request, student):
+    
+    allQuestions = list(Question.objects.all())
+    
+    questions = random.sample(allQuestions, 2)
+    
+    pairs = []
+    
+    for quest in questions:
+         answers = quest.answer_set.all()
+         
+         pair = {
+             "question" : quest,
+             "answers" : answers
+             }
+         
+         pairs.append(pair)
+         
+    context = {"pairs" : pairs}
+    
+    urlTemplate = ""
+
+    if student == 1:
+        urlTemplate = 'quiz/newAttemptLorenzo.html'
+    if student == 2:
+        urlTemplate = 'quiz/newAttemptLuca.html'
+    if student == 3:
+        urlTemplate = 'quiz/newAttemptTommaso.html'
+    
+    return render(request, urlTemplate, context);
